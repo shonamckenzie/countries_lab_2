9a2f7ce11df292aa026311d9332a279ec9888d8f
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-11 15:53:34 +0100

    list created

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+<div>
+<h1>Countries</h1>
+<countries-list :countries='countries'></countries-list>
+</div> 
+</template>
+
+<script>
+import CountriesList from './components/CountriesList.vue';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted() {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  },
+  components: {
+    "countries-list": CountriesList
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,17 @@
+<template lang='html'>
+<li>{{ country.name }}</li>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'list-item',
+    props: ['country']
+
+}
+</script>
+
+<style lang='css' scoped>
+
+</style>
